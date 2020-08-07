30f59cb73d8512939b3521c288fe233fea0f7b23
renemonteiro <renemonteiro@hotmail.com.br>
2020-08-07 13:11:27 -0300

    exercicios-de-interpretação

---
+++ semana4/lista-de-exercicios/script.js
@@ -0,0 +1,28 @@
+/*
+Lista de exercícios:
+
+Interpretação de Código
+
+1. A constante meuDinhero invoca a função "ConversorDeMoeda", que recebe um parametro 
+"valorEmDolar". Na variável "cotação" armazenamos a cotaçãodo Dólar com tipo numero, em 
+seguida retorna um o parametro recebido "100" multiplicado pela cotação. Por fim a impressão 
+da variável.
+
+
+2. A variável "novoMontante" invoca a função "investeDinheiro" com os parametros "ações e 150". 
+Quando a função identifica que a string recebida é igual a uma condição estabelecida ("Ações")
+será multiplicado o numero recebido como parametro multiplicado por 1.1 e armazenado na 
+variável valorAposInvestimento. Já na opção "tesouro direto" não será armazenado pois não atente
+as condições do switch case e imprimirá "tipo de investimento incorreto".
+
+
+3. São 3 impressoes em que a primeira informa o total de elementos do "array numeros", o segundo o total de um
+"array1" onde são armazenados os numeros pares e o ultimo armazena os impares. O "for" faz toda divisão de
+armazenamento das variáveis oriundos do arrey numeros.
+
+
+4. O for utiliza os indices do array "numeros" se o elemento for menor que a variável "numero1 == infinity" o elemento 
+vai sobrescrever o valor da variável "numero1". O mesmo acontece no proximo "if" porem com o comparador invertido (maior que) 
+Se o numero for maior que "numero2" ele vai sobrescrever a variável.
+
+*/
