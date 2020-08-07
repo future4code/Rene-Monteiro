d3ca1bd6106e7de700f299f58072d087c4bedc36
renemonteiro <renemonteiro@hotmail.com.br>
2020-08-07 15:14:02 -0300

    logica-de-programação

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -1,7 +1,7 @@
 /*
 Lista de exercícios:
 
-Interpretação de Código
+-Interpretação de Código
 
 1. A constante meuDinhero invoca a função "ConversorDeMoeda", que recebe um parametro 
 "valorEmDolar". Na variável "cotação" armazenamos a cotaçãodo Dólar com tipo numero, em 
@@ -26,3 +26,131 @@ vai sobrescrever o valor da variável "numero1". O mesmo acontece no proximo "if
 Se o numero for maior que "numero2" ele vai sobrescrever a variável.
 
 */
+//-Lógica de programação
+
+//1. Utilizando o for, each(), map() e filter()
+
+let lista = [1,2,3,4,5,6,7,8,9,10]
+
+let callback = function par(elemento, index, array) {
+      if (elemento % 2 == 0){
+          let numero = elemento
+          return numero
+      }
+    
+}
+
+let numerosPares = lista.filter(callback)
+console.log(numerosPares)
+
+
+//2. 
+//a) false
+//b) false
+//c) true
+//d) false
+//e) true
+
+//3.
+
+let numero = 20
+
+let array = []
+
+while(array.length < numero){
+    
+    for (let i = 0; i < numero*2; i++){
+        if(i % 2 == 0){
+            array.push(i)
+        }
+
+
+    }
+
+
+
+}
+console.log(array)
+
+
+
+//4.
+
+function classificaTriangulo(a, b, c){
+ 
+    if (a == b && a == c ) {
+        return `O triangulo é equilatero`
+    } else if (a == b && a !== c  || b == c && b !== a || c == a && c !== b) {
+
+        return `O triangulo é isósceles`
+
+    }else if (a !== b && a !== c ) {
+
+        return `o triangulo é escaleno`
+    }
+}
+
+
+let resposta = classificaTriangulo(1,1,1)
+console.log(resposta)
+
+
+
+//5.
+
+function exercicioCinco(a, b) {
+    numeroMaior(a,b) 
+    numeroDivisivel(a,b)
+    diferenca(a,b)
+
+        //i.
+        function numeroMaior(a,b) {
+                if (a > b){
+                    console.log(`O maior é: ${a}`)
+            } else {
+                console.log(`O maior é: ${b}`)
+                
+            }    
+        }  
+        //ii.
+        function diferenca(a,b) {
+            if (a > b) {
+                console.log(a-b)
+            } else {
+                console.log(b-a)
+            }
+            
+        }
+    
+        // iii.
+        function numeroDivisivel(a, b) {
+            if (a % b == 0) {
+                console.log( `${a} é divisivel por ${b}`)
+            } else {
+                console.log(`${a} NÃO é divisivel por ${b}`)
+            
+            } 
+        
+            if (b % a == 0) {
+            console.log( `${b} é divisivel por ${a}`)
+            
+        } else  {
+            console.log( `${b} NÃO é divisivel por ${a}`)
+        }
+    
+    }
+
+
+}
+
+
+exercicioCinco(10,200)
+
+
+
+
+
+
+
+
+
