6648bb47d0b4382a0de2fe80a25813bb26e44faa
renemonteiro <renemonteiro@hotmail.com.br>
2020-11-11 16:56:44 -0300

    entrega parcial

---
+++ semana18/servicosBackend-aula-52/src/endpoints/editUser.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import updateUser from "../data/updateUser";
+import { AuthenticationData, getTokenData } from "../services/authenticator";
+
+export default async function editUser(
+   req: Request,
+   res: Response
+) {
+   try {
+
+      const token:string = req.headers.authorization as string
+      const tokenData: AuthenticationData = getTokenData(token)
+
+      if(tokenData.role != 'ADMIN') {
+         res.statusCode = 401
+         throw new Error("Não autorizado")
+      }
+      if (
+         req.body.name === '' ||
+         req.body.nickname === '' ||
+         req.body.email === ''
+      ) {
+         throw new Error("Nenhum dos campos pode estar em branco")
+      }
+
+      if (!req.body.name && !req.body.nickname && !req.body.email) {
+         throw new Error("Escolha ao menos um valor para alterar")
+      }
+
+      await updateUser(
+         tokenData.id,
+         req.body.name,
+         req.body.nickname,
+         req.body.email
+      )
+
+
+      res.status(200).send({
+         message: "Usuário atualizado!"
+      })
+
+   } catch (error) {
+      res.status(400).send({
+         message: error.message || error.sqlMessage
+      })
+   }
+}
